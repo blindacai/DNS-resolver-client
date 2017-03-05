aca2579cf6f3cef57c4508dab38b13df88d04ca4
blindacai <linda_cai2014@hotmail.com>
2017-03-04 22:28:00 -0800

    send InitialQuery.bin to some DNS

--- testMain.java
+++ testMain.java
@@ -1,15 +1,25 @@
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by linda on 3/4/2017.
  */
 public class testMain {
+    public static final String PATH = "D:\\UBC\\2016 term 2\\cpsc 317\\assignment\\a2\\DNSInitialQuery.bin";
+    public static final String DNS = "198.162.35.1";
 
     public static void main(String[] args) throws IOException {
         DatagramSocket clientSocket = new DatagramSocket();
-        InetAddress address = InetAddress.getByName("199.7.83.42");
-        byte[] buf = new byte[256];
+        InetAddress address = InetAddress.getByName(DNS);
+
+        Path path = Paths.get(PATH);
+        byte[] buf = Files.readAllBytes(path);  // to be replaced by encoded user input
+
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 53);
         clientSocket.send(packet);
     }
