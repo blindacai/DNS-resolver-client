0c7d64743f81f0eaac2734052bf2d09e353c1bba
blindacai <linda_cai2014@hotmail.com>
2017-03-04 23:06:01 -0800

    print DNS response to screen

--- testMain.java
+++ testMain.java
@@ -1,3 +1,4 @@
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -12,6 +13,7 @@ import java.nio.file.Paths;
 public class testMain {
     public static final String PATH = "D:\\UBC\\2016 term 2\\cpsc 317\\assignment\\a2\\DNSInitialQuery.bin";
     public static final String DNS = "198.162.35.1";
+    public static final int MAX_LEN = 512;
 
     public static void main(String[] args) throws IOException {
         DatagramSocket clientSocket = new DatagramSocket();
@@ -22,5 +24,17 @@ public class testMain {
 
         DatagramPacket packet = new DatagramPacket(buf, buf.length, address, 53);
         clientSocket.send(packet);
+
+        // to receive a response
+        byte[] buftwo = new byte[MAX_LEN];
+        packet = new DatagramPacket(buftwo, buftwo.length);
+        clientSocket.receive(packet);
+
+        byte[] response = packet.getData();
+        ByteArrayInputStream readbyte = new ByteArrayInputStream(response);
+        int reader;
+        while((reader = readbyte.read())!= -1){
+            System.out.println(Integer.toHexString(reader));
+        }
     }
 }
